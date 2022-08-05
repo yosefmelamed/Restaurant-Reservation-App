9e97567260bf75f3e69f4ff2098bdc96e723ac41
Yosef Melamed <yosefmelamed112@gmail.com>
2022-08-05 12:01:57 -0600

    fixed-mobile-look

--- front-end/src/reservations/erservationList-mobile.js
+++ front-end/src/reservations/erservationList-mobile.js
@@ -1,7 +1,6 @@
 import Seats from "./reservationSeats";
 import EditButton from "./editButton";
 import Cancel from "./reservationsCancel";
-import BasicExample from "./accordion";
 function MobileList({ reservations }) {
   function convertTo12Hour(oldFormatTime) {
     var oldFormatTimeArray = oldFormatTime.split(":");
@@ -20,114 +19,76 @@ function MobileList({ reservations }) {
     var newFormatTime = `${hours}:${min} ${AMPM}`;
     return newFormatTime;
   }
-  return reservations.map((reservation) => (
-    <>
-      <table className="table-sm d-flex justify-content-center  d-sm-none">
-        <div className="accordion" id="accordionExample">
-          <div className="accordion-item">
-            <h2 className="accordion-header" id="headingOne">
-              <button
-                className="accordion-button btn btn-primary"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#collapseOne"
-                aria-expanded="true"
-                aria-controls="collapseOne"
-              >
-                {`Reservation Id: ${reservation.reservation_id}`}
-              </button>
-            </h2>
-            <div
-              id="collapseOne"
-              className="accordion-collapse collapse show"
-              aria-labelledby="headingOne"
-              data-bs-parent="#accordionExample"
+  return reservations.map((reservation, i) => (
+    <ul key={i} className="table-sm d-flex justify-content-center  d-sm-none">
+      <div className="accordion" id="accordionExample">
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="headingOne">
+            <button
+              className="accordion-button btn btn-primary"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseOne"
+              aria-expanded="true"
+              aria-controls="collapseOne"
             >
-              <tbody className="d-flex justify-content-center">
-                <tr key={reservation.reservation_id}>
-                  <div className="accordion-body">
-                    <td>Time:</td>
-                    <td>{convertTo12Hour(reservation.reservation_time)}</td>
-                  </div>
-                </tr>
-              </tbody>
-              <tbody className="d-flex justify-content-center">
-                <tr key={reservation.reservation_id}>
-                  <div className="accordion-body">
-                    <td>Date:</td>
-                    <td>{reservation.reservation_date}</td>
-                  </div>
-                </tr>
-              </tbody>
-              <tbody className="d-flex justify-content-center">
-                <tr key={reservation.reservation_id}>
-                  <div className="accordion-body">
-                    <td>First-Name:</td>
-                    <td>{reservation.first_name}</td>
-                  </div>
-                </tr>
-              </tbody>
-              <tbody className="d-flex justify-content-center">
-                <tr key={reservation.reservation_id}>
-                  <div className="accordion-body">
-                    <td>Last-Name:</td>
-                    <td>{reservation.last_name}</td>
-                  </div>
-                </tr>
-              </tbody>
-              <tbody className="d-flex justify-content-center">
-                <tr key={reservation.reservation_id}>
-                  <div className="accordion-body">
-                    <td>Party-Size:</td>
-                    <td>{reservation.people}</td>
-                  </div>
-                </tr>
-              </tbody>
-              <tbody className="d-flex justify-content-center">
-                <tr key={reservation.reservation_id}>
-                  <div className="accordion-body">
-                    <td>Mobile-Number:</td>
-                    <td>{reservation.mobile_number}</td>
-                  </div>
-                </tr>
-              </tbody>
-              <tbody className="d-flex justify-content-center">
-                <tr key={reservation.reservation_id}>
-                  <div className="accordion-body">
-                    <td>Status:</td>
-                    <td data-reservation-id-status={reservation.reservation_id}>
-                      {reservation.status}
-                    </td>
-                  </div>
-                </tr>
-              </tbody>
-              <tbody className="d-flex justify-content-center">
-                <tr key={reservation.reservation_id}>
-                  <div className="accordion-body">
-                    {reservation.status === "booked" && (
-                      <>
-                        <td>
-                          <Seats reservation_id={reservation.reservation_id} />
-                        </td>
-                        <td>
-                          <EditButton
-                            reservation_id={reservation.reservation_id}
-                          />
-                        </td>
-                        <td>
-                          <Cancel reservation={reservation} />
-                        </td>
-                      </>
-                    )}
-                  </div>
-                </tr>
-              </tbody>
-            </div>
+              {`Reservation Id: ${reservation.reservation_id}`}
+            </button>
+          </h2>
+          <div
+            id="collapseOne"
+            className="accordion-collapse collapse show"
+            aria-labelledby="headingOne"
+            data-bs-parent="#accordionExample"
+          >
+            <li className="d-flex justify-content-center accordion-body">
+              Time:
+              {convertTo12Hour(reservation.reservation_time)}
+            </li>
+
+            <li className="d-flex justify-content-center accordion-body">
+              Date:
+              {reservation.reservation_date}
+            </li>
+
+            <li className="d-flex justify-content-center accordion-body">
+              First-Name:
+              {reservation.first_name}
+            </li>
+            <li className="d-flex justify-content-center accordion-body">
+              Last-Name:
+              {reservation.last_name}
+            </li>
+            <li className="d-flex justify-content-center accordion-body">
+              Party-Size:
+              {reservation.people}
+            </li>
+            <li className="d-flex justify-content-center accordion-body">
+              Mobile-Number:
+              {reservation.mobile_number}
+            </li>
+            <li
+              className="d-flex justify-content-center accordion-body"
+              data-reservation-id-status={reservation.reservation_id}
+            >
+              Status:
+              {reservation.status}
+            </li>
+            <li className="d-flex justify-content-center accordion-body">
+              {reservation.status === "booked" && (
+                <>
+                  <Seats reservation_id={reservation.reservation_id} />
+
+                  <EditButton reservation_id={reservation.reservation_id} />
+
+                  <Cancel reservation={reservation} />
+                </>
+              )}
+            </li>
           </div>
         </div>
-      </table>
-      <BasicExample />
-    </>
+      </div>
+    </ul>
   ));
 }
 
