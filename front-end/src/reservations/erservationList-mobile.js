1c8f060eda5413c5381ef4009ca6cd088f26a751
Yosef Melamed <yosefmelamed112@gmail.com>
2022-08-15 12:32:07 -0600

    fixedKeysError

--- front-end/src/reservations/erservationList-mobile.js
+++ front-end/src/reservations/erservationList-mobile.js
@@ -1,6 +1,7 @@
 import Seats from "./reservationSeats";
 import EditButton from "./editButton";
 import Cancel from "./reservationsCancel";
+import React from "react";
 
 //hanldles the design change of the reservations list for a mobile phone
 function MobileList({ reservations }) {
@@ -22,13 +23,13 @@ function MobileList({ reservations }) {
     return newFormatTime;
   }
   return reservations.map((reservation) => (
-    <>
+    <React.Fragment key={reservation.reservation_id}>
       <ol
-        key={reservation.reservation_id}
         className="container-fluid d-block justify-content-center d-lg-none"
         style={{ listStyle: "none", textAlign: "center" }}
       >
         <li
+          key="reservationNumber"
           style={{
             fontSize: "18px",
             fontWeight: "bold",
@@ -38,18 +39,23 @@ function MobileList({ reservations }) {
         >
           Reservation #{reservation.reservation_id}
         </li>
-        <li>Time: {convertTo12Hour(reservation.reservation_time)}</li>
+        <li key="time">
+          Time: {convertTo12Hour(reservation.reservation_time)}
+        </li>
 
-        <li>Date: {reservation.reservation_date}</li>
+        <li key="date">Date: {reservation.reservation_date}</li>
 
-        <li>First-Name: {reservation.first_name}</li>
-        <li>Last-Name: {reservation.last_name}</li>
-        <li>Party-Size: {reservation.people}</li>
-        <li>Mobile-Number: {reservation.mobile_number}</li>
-        <li data-reservation-id-status={reservation.reservation_id}>
+        <li key="firstName">First-Name: {reservation.first_name}</li>
+        <li key="lastName">Last-Name: {reservation.last_name}</li>
+        <li key="partySize">Party-Size: {reservation.people}</li>
+        <li key="mobileNumber">Mobile-Number: {reservation.mobile_number}</li>
+        <li
+          key="status"
+          data-reservation-id-status={reservation.reservation_id}
+        >
           Status: {reservation.status}
         </li>
-        <li>
+        <li key="buttons">
           {reservation.status === "booked" && (
             <>
               <Seats reservation_id={reservation.reservation_id} />
@@ -70,7 +76,7 @@ function MobileList({ reservations }) {
           marginTop: "70px",
         }}
       ></hr>
-    </>
+    </React.Fragment>
   ));
 }
 
--- front-end/src/tables/tableListMobile.js
+++ front-end/src/tables/tableListMobile.js
@@ -2,49 +2,50 @@ import React from "react";
 //hanldles the design change of the tables list for a mobile phone
 export default function TablesMobileList({ tables, resetHandler }) {
   return tables.map((table) => (
-    <>
-      <ol
-        key={table.table_id}
-        className="container-fluid d-block justify-content-center d-lg-none"
-        style={{ listStyle: "none", textAlign: "center" }}
+    <ol
+      key={table.table_id}
+      className="container-fluid d-block justify-content-center d-lg-none"
+      style={{ listStyle: "none", textAlign: "center" }}
+    >
+      <li
+        key="table"
+        style={{
+          fontSize: "18px",
+          fontWeight: "bold",
+          textAlign: "center",
+          marginTop: "30px",
+        }}
       >
-        <li
-          style={{
-            fontSize: "18px",
-            fontWeight: "bold",
-            textAlign: "center",
-            marginTop: "30px",
-          }}
-        >
-          Table: {table.table_id}
-        </li>
+        Table: {table.table_id}
+      </li>
 
-        <li>Table Name: {table.table_name}</li>
+      <li key="tableName">Table Name: {table.table_name}</li>
 
-        <li>Capacity: {table.capacity}</li>
+      <li key="capacity">Capacity: {table.capacity}</li>
 
-        <li data-table-id-status={table.table_id}>
-          Status: {table.reservation_id ? "Occupied" : "Free"}
+      <li data-table-id-status={table.table_id} key="status">
+        Status: {table.reservation_id ? "Occupied" : "Free"}
+      </li>
+      {table.reservation_id && (
+        <li key="occupiedBy">
+          Occupied By: Reservation #{table.reservation_id}
         </li>
-        {table.reservation_id && (
-          <li>Occupied By: Reservation #{table.reservation_id}</li>
-        )}
-        <li>
-          <>
-            {table.reservation_id && (
-              <button
-                type="button"
-                className="btn btn-secondary mr-2 my-2 mb-5"
-                data-table-id-finish={table.table_id}
-                value={[table.table_id, table.reservation_id]}
-                onClick={resetHandler}
-              >
-                Finish
-              </button>
-            )}
-          </>
-        </li>
-      </ol>
-    </>
+      )}
+      <li key="button">
+        <>
+          {table.reservation_id && (
+            <button
+              type="button"
+              className="btn btn-secondary mr-2 my-2 mb-5"
+              data-table-id-finish={table.table_id}
+              value={[table.table_id, table.reservation_id]}
+              onClick={resetHandler}
+            >
+              Finish
+            </button>
+          )}
+        </>
+      </li>
+    </ol>
   ));
 }
