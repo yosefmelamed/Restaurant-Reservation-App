b620aef77d74aed89b2d05b302113a7168e22e21
Yosef Melamed <yosefmelamed112@gmail.com>
2023-01-02 13:08:15 -0700

    trying to fix dotenv2

--- front-end/src/dashboard/backgroundPicture.js
+++ front-end/src/dashboard/backgroundPicture.js
@@ -1,24 +1,24 @@
 export default function BackgroungPictures() {
   return (
-    <div className="d-flex  justify-content-between">
-      <div>
+    <div className="d-block">
+      <div className="d-flex justify-content-between">
         <img
-          src="https://media.istockphoto.com/photos/table-scene-of-assorted-take-out-or-delivery-foods-top-down-view-on-a-picture-id1232401725?k=20&m=1232401725&s=612x612&w=0&h=TYoqO_29JQk9pe6AX7yrzggBr2jK-y-_5ySKIFLa1GY="
-          alt="picture-of-steak"
+          src="https://media.istockphoto.com/id/692557700/photo/burger.jpg?s=612x612&w=0&k=20&c=8ajzPWqvrLvl779OI3XMFR_tZV5V-7VSDlSeaX47Uhk="
+          alt="burger"
+          className="d-none d-md-block"
+        />
+        <img
+          src="https://media.istockphoto.com/id/1182604271/photo/festive-table-setting-food-and-drinks-plates-and-glasses-evening-lights-and-candles-new-years.jpg?s=612x612&w=0&k=20&c=KD_jshmjMrrFna4ruziFk14ILZxd_82L_5T2bR9TOu0="
+          alt="restaurant"
         />
       </div>
-      <div>
-        <img
-          src="https://media.istockphoto.com/photos/vegan-food-grilled-vegetables-in-a-cooking-pan-overhead-view-with-picture-id1273297972?k=20&m=1273297972&s=612x612&w=0&h=ih9lN1li4mm9Sh_PBLSBn3j-hlwJ9R_l1QUobrD1m7c="
-          alt="picture-of-vegeatbles"
-        />
-      </div>
-      <div>
+
+      {/* <div>
         <img
           src="https://media.istockphoto.com/photos/burgers-and-sandwich-with-light-beer-picture-id1045929570?k=20&m=1045929570&s=612x612&w=0&h=8jfdqvNvbIlGyuCfSKXQIX8IhFIdqGu8JRsFCq14-tE="
           alt="picture-of-salmon"
         />
-      </div>
+      </div> */}
     </div>
   );
 }
