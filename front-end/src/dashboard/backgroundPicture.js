167f87c3ee919a8daccc2a76eb78c8d1dd8daf48
Yosef Melamed <yosefmelamed112@gmail.com>
2023-01-02 13:22:58 -0700

    deleted package.json

--- front-end/src/dashboard/backgroundPicture.js
+++ front-end/src/dashboard/backgroundPicture.js
@@ -1,17 +1,16 @@
 export default function BackgroungPictures() {
   return (
     <div className="d-block">
-      <div className="d-flex justify-content-between">
-        <img
-          src="https://media.istockphoto.com/id/692557700/photo/burger.jpg?s=612x612&w=0&k=20&c=8ajzPWqvrLvl779OI3XMFR_tZV5V-7VSDlSeaX47Uhk="
-          alt="burger"
-          className="d-none d-md-block"
-        />
-        <img
-          src="https://media.istockphoto.com/id/1182604271/photo/festive-table-setting-food-and-drinks-plates-and-glasses-evening-lights-and-candles-new-years.jpg?s=612x612&w=0&k=20&c=KD_jshmjMrrFna4ruziFk14ILZxd_82L_5T2bR9TOu0="
-          alt="restaurant"
-        />
-      </div>
+      <img
+        src="https://media.istockphoto.com/id/692557700/photo/burger.jpg?s=612x612&w=0&k=20&c=8ajzPWqvrLvl779OI3XMFR_tZV5V-7VSDlSeaX47Uhk="
+        alt="burger"
+        className="d-none d-md-block"
+      />
+
+      <img
+        src="https://media.istockphoto.com/id/1182604271/photo/festive-table-setting-food-and-drinks-plates-and-glasses-evening-lights-and-candles-new-years.jpg?s=612x612&w=0&k=20&c=KD_jshmjMrrFna4ruziFk14ILZxd_82L_5T2bR9TOu0="
+        alt="restaurant"
+      />
 
       {/* <div>
         <img
